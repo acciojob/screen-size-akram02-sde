64a0a3906234341083ba3c3cba6bcd0f7cf5f045
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-21 11:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+const container = document.getElementById("sizeInfo");
+let x = 0;
+let y = 0;
+
+renderXY();
+
+function renderXY() {
+	container.innerText = ""
+	let str = document.createElement("h1");
+	str.innerText = `Width: ${x} and Height: ${y}`;
+	container.append(str);
+}
+
+window.addEventListener("resize", (e) => {
+	x = window.innerWidth;
+	y = window.innerHeight;
+	renderXY()
+})
+
+
