c25b6e3c60dfbfa7250b65d04dd71c6632829b21
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-07-20 23:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const container = document.getElementById("sizeInfo");
-let x = window.innerWidth;
-let y = window.innerHeight;
-
-renderXY();
-
-function renderXY() {
-	container.innerText = ""
-	let str = document.createElement("h1");
-	str.innerText = `Width: ${x} and Height: ${y}`;
-	container.append(str);
-}
-
-window.addEventListener("resize", (e) => {
-	x = window.innerWidth;
-	y = window.innerHeight;
-	renderXY()
-})
-
-
+//your JS code here. If required.
+//your JS code here. If required.
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
